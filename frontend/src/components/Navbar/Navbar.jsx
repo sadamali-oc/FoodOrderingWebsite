1c05a6a8a65b96fb84a1a4bdd73b80799be8945f
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-05-28 02:25:28 +0530

    navbar changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { userState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <img src={assets.logo}/>
-    </div>
-  )
-}
+  const [menu, setmenu] = userState("home");
 
-export default Navbar
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li className={menu === "home" ? "active" : ""}>Home</li>
+        <li className={menu === "menu" ? "active" : ""}> menu</li>
+
+        <li className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+        <li className={menu === "contact-us" ? "active" : ""}>contact us</li>
+      </ul>
+
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+
+        <img src={assets.basket_icon} alt="" />
+        <div className="navbar-search-icon">
+          <div className="dot"></div>
+          <button>sign in</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
