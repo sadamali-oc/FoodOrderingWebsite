0031ea21fd7e42a1d16cd457773cc85460eef991
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-05-28 00:25:35 +0530

    changes

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={assets.logo}/>
+    </div>
+  )
+}
+
+export default Navbar
