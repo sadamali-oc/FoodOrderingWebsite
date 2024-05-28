be18c1c619e04dcb5ce2d7bf322f46f0625e2d7f
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-05-28 19:49:53 +0530

    get the food details

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter, Router} from 'react-router-dom'
+import StoreContextProvider from './components/context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
 <BrowserRouter>
+<StoreContextProvider>
 <App />
+</StoreContextProvider>
+
 </BrowserRouter>
 
    
