1ed92534bee6fd8abf5ec9e3a47596f874078ce8
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-05-28 14:10:58 +0530

    Explore Menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import PlaceHolder from './pages/PlaceOrder/PlaceHolder'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart'; // Ensure you import the Cart component
+import PlaceHolder from './pages/PlaceOrder/PlaceHolder';
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/' element={<Cart/>} />
-
-        <Route path='/' element={<PlaceHolder/>} />
-
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/place-order' element={<PlaceHolder />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div className='header'></div>
-  )
-}
+    <div className="header">
+      <div className="header-contents">
+        <h2> Order your favourite food</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients is to satisy your craving and elevate your dining
+          experience, one delicious meal at a time.{" "}
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
